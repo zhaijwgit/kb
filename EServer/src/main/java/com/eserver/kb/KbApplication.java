0f5ddffe2883530d7110e8c8b129fcdb653ded91
zhaijw <zhaijiweiat@163.com>
2018-01-04 16:51:09 +0800

    commit EServer

---
+++ EServer/src/main/java/com/eserver/kb/KbApplication.java
@@ -0,0 +1,12 @@
+package com.eserver.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbApplication.class, args);
+	}
+}
