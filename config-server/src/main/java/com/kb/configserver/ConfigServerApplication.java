7fe4311d43f65de6b1f47ca987802ed2027ecdb8
zhaijw <zhaijiweiat@163.com>
2018-01-10 09:06:39 +0800

    add respo

--- config-server/src/main/java/com/kb/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/kb/configserver/ConfigServerApplication.java
@@ -2,9 +2,10 @@ package com.kb.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
